1c1122a5b322a6905534ed3d4ee9cdb529cd3755
Zeynep Derbent <57301676+zeynep394@users.noreply.github.com>
2021-11-09 00:48:04 +0300

    Update and rename 1.1 Is Unique.py to is_unique.java

---
+++ 1- Arrays and Strings/is_unique.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void is_unique(String word){
+        char[] wordArray = word.toCharArray();
+        ArrayList<Character>  arrayList = new ArrayList<Character>();
+        arrayList.add(wordArray[0]);
+        
+        boolean unique = true;
+        for(int i =1; i<wordArray.length;i++){
+            if(wordArray[i]==' '){
+                continue;
+            }
+
+
+                for(int j = 0; j<arrayList.size();j++) {
+                if(wordArray[i] == arrayList.get(j)){
+                    System.out.println("false");
+                    System.out.print("The letter '"+wordArray[i]+"' is already in String");
+                    unique = false;
+                }
+                }
+
+            if(unique == false){
+                break;
+            }else{
+                arrayList.add(wordArray[i]);
+                //System.out.println(arrayList.size());
+            }
+        }
+    }
+    public static void main(String[] args) {
+	// write your code here
+        is_unique("Legolas bir elftir");
+    }
+}
+
